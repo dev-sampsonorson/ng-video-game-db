d3ae3a0cc90ff4de974ab95a3c5d2682b58739e7
Sampson Orson Jackson <sampson.orson@gmail.com>
2021-07-13 16:38:58 +0100

    completed

---
+++ src/app/interceptors/http-headers.intercepter.ts
@@ -0,0 +1,25 @@
+import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { Observable } from "rxjs";
+
+
+
+@Injectable()
+export class HttpHeadersInterceptor implements HttpInterceptor {
+  constructor() { }
+
+  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    req = req.clone({
+      setHeaders: {
+        'x-rapidapi-key': 'esGbwrm390mshS2BCl0RALxQRtZTp1W7sFMjsnyJlJzDXVkW0H',
+        'x-rapidapi-host': 'rawg-video-games-database.p.rapidapi.com'
+      },
+      setParams: {
+        key: '68bc295bf28f4b8ca76c73ffd47b475e'
+      }
+    });
+
+    return next.handle(req);
+  }
+
+}
